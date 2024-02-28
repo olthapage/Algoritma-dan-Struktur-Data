7b9bf80740c33b7e7a5acc1a8ac204203bb07a85
olthapage <olthar.rosyeda01@gmail.com>
2024-02-28 23:16:28 +0700

    P3_Percobaan3_25_Pertanyaan4

--- src/P3/ArraySegitiga.java
+++ src/P3/ArraySegitiga.java
@@ -13,6 +13,20 @@ public class ArraySegitiga {
             System.out.println("Keliling Segitiga " + i + ": " + b2Array[i].hitungKeliling());
         }
 
+        System.out.println("================================================================================");
+
+        Segitiga[] sgArray = new Segitiga[4];
+        sgArray[0] = new Segitiga(10, 4);
+        sgArray[1] = new Segitiga(20, 10);
+        sgArray[2] = new Segitiga(15, 6);
+        sgArray[3] = new Segitiga(25, 10);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Luas Segitiga ke " + i + ": " + sgArray[i].hitungLuas());
+            System.out.println("Keliling Segitiga " + i + ": " + sgArray[i].hitungKeliling());
+            
+        }
+
     }
     
 }
