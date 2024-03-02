420b710b09b5192ce63bd6b79e46f63f965997d9
olthapage <olthar.rosyeda01@gmail.com>
2024-03-02 11:39:44 +0700

    P3_Latihan Praktikum_Pertanyaan3_26

--- src/P3/ArrayMahasiswa.java
+++ src/P3/ArrayMahasiswa.java
@@ -6,14 +6,14 @@ public class ArrayMahasiswa {
     public static void main(String[] args) {
         InformasiMahasiswa[] mhArray = new InformasiMahasiswa[3];
         Scanner sc = new Scanner(System.in);
-
+        
         for (int i = 0; i < 3; i++) {
             mhArray[i] = new InformasiMahasiswa();
             System.out.println("Masukkan data mahasiswa ke-" + (i+1));
             System.out.print("Masukkan nama mahasiswa: ");
             mhArray[i].nama = sc.next();
             System.out.print("Masukkan nim : ");
-            mhArray[i].nim = sc.nextDouble();
+            mhArray[i].nim = sc.nextInt();
             System.out.print("Masukkan jenis kelamin (L/P): ");
             mhArray[i].jenisKelamin = sc.next().charAt(0);       
             System.out.print("Masukkan IPK : ");
@@ -22,10 +22,30 @@ public class ArrayMahasiswa {
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i+1));
-            System.out.println("nama : " + mhArray[i].nama + "nim : " + mhArray[i].nim + "jenis kelamin : "
-                    + mhArray[i].jenisKelamin + "nilai IPK : " + mhArray[i].ipk);
+            System.out.println("nama : " + mhArray[i].nama);
+            System.out.println("nim : " + mhArray[i].nim);
+            System.out.println("jenis kelamin : " + mhArray[i].jenisKelamin);
+            System.out.println("nilai IPK : " + mhArray[i].ipk);          
+            
         }
+        System.out.println();
 
+        InformasiMahasiswa mahasiswa = new InformasiMahasiswa();
+        double ipkTerbesar = mahasiswa.IPKTerbesar(mhArray);
+
+        System.out.println("Nilai IPK Terbesar : " + ipkTerbesar);
+        System.out.println("Data Mahasiswa dengan IPK Terbesar:");
+        for (int i = 0; i < 3; i++) {
+            if (mhArray[i].ipk == ipkTerbesar) {
+                System.out.println("Nama : " + mhArray[i].nama);
+                System.out.println("NIM : " + mhArray[i].nim);
+                System.out.println("Jenis Kelamin : " + mhArray[i].jenisKelamin);
+                System.out.println("Nilai IPK : " + mhArray[i].ipk);
+            }
+        }
+        System.out.println();
+        
+        double rataRataIPK = mahasiswa.rataRataIPK(mhArray);
+        System.out.println("Rata-rata IPK : " + rataRataIPK);
     }
-
 }
--- src/P3/InformasiMahasiswa.java
+++ src/P3/InformasiMahasiswa.java
@@ -2,7 +2,44 @@ package P3;
 
 public class InformasiMahasiswa {
     public String nama;
-    public double nim;
+    public int nim;
     public char jenisKelamin;
-    public double ipk;  
+    public double ipk;
+
+    public InformasiMahasiswa(String nama, int nim, char jenisKelamin, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jenisKelamin = jenisKelamin;
+        this.ipk = ipk;
+    }
+
+    public InformasiMahasiswa() {
+        //TODO Auto-generated constructor stub
+    }
+
+    public double IPKTerbesar(Object[] mhArray) {
+        double ipkTerbesar = ((InformasiMahasiswa) mhArray[0]).ipk;
+        for (int i = 1; i < mhArray.length; i++) {
+            if (((InformasiMahasiswa) mhArray[i]).ipk > ipkTerbesar) {
+                ipkTerbesar = ((InformasiMahasiswa) mhArray[i]).ipk;
+                
+            }
+        }
+        return ipkTerbesar;
+    }
+
+    public double rataRataIPK(InformasiMahasiswa[] mhArray) {
+        double totalIPK = 0;
+        for (int i = 0; i < mhArray.length; i++) {
+            totalIPK += mhArray[i].ipk;
+        }
+        return totalIPK / mhArray.length;
+    }
+    
+
+    
 }
+
+
+
+
