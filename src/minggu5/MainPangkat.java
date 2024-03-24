2ade77ec52d929f4a5e4d1e7c9219cedce454739
olthapage <olthar.rosyeda01@gmail.com>
2024-03-24 13:28:49 +0700

    minggu5_Percobaan2_Pertanyaan4_25

--- src/minggu5/MainPangkat.java
+++ src/minggu5/MainPangkat.java
@@ -17,17 +17,30 @@ public class MainPangkat {
             int pangkat = sc.nextInt();
             png[i] = new Pangkat(nilai, pangkat);
         }
-        
-        System.out.println("HASIL PANGKAT - BRUTE FORCE");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai+ " pangkat " + png[i].pangkat+
-                                " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-        }
 
-        System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai+ " pangkat " + png[i].pangkat+
-                                " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        System.out.println("Pilih metode penghitungan:");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide and Conquer");
+        System.out.println("Masukkan pilihan Anda: ");
+        int pilihan = sc.nextInt();
+        
+        switch (pilihan) {
+            case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat +
+                            " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat +
+                            " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
         }
     }
 }
