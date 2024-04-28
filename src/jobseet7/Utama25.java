3df1b0bdc9f6d98b4db252c07e30e956caa57e2f
olthapage <olthar.rosyeda01@gmail.com>
2024-04-28 21:23:29 +0700

    P7_Percobaan1_Pertanyaan4_25

--- src/jobseet7/Utama25.java
+++ src/jobseet7/Utama25.java
@@ -4,29 +4,33 @@ import java.util.Scanner;
 
 public class Utama25 {
     public static void main(String[] args) {
-        Gudang25 gudang = new Gudang25(7);
-        Scanner sc25 = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas gudang: ");
+        int kapasitasGudang = sc.nextInt();
+        Gudang25 gudang = new Gudang25(kapasitasGudang);
 
         while (true) {
             System.out.println("\nMenu:");
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
-            System.out.print("Pilih operasi: "); 
-            int pilihan = sc25.nextInt();
-            sc25.nextLine();
+            System.out.println("4. Lihat barang teratas");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih operasi: ");
+            int pilihan = sc.nextInt();
+            sc.nextLine();
 
             switch (pilihan) {
                 case 1:
                     System.out.print("Masukkan kode barang: ");
-                    int kode = sc25.nextInt();
-                    sc25.nextLine();
+                    int kode = sc.nextInt();
+                    sc.nextLine();
                     System.out.print("Masukkan nama barang: ");
-                    String nama = sc25.nextLine(); 
-                    System.out.print("Masukkan nama kategori: "); 
-                    String kategori = sc25.nextLine(); 
-                    Barang25 barangBaru = new Barang25(kode, nama, kategori); 
+                    String nama = sc.nextLine();
+                    System.out.print("Masukkan nama kategori: ");
+                    String kategori = sc.nextLine();
+                    Barang25 barangBaru = new Barang25(kode, nama, kategori);
                     gudang.tambahBarang(barangBaru);
                     break;
                 case 2:
@@ -36,7 +40,10 @@ public class Utama25 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
-                    System.exit(0); 
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Pilihan tidak valid. Silahkan coba lagi.");
