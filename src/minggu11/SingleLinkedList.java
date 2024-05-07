34b7c68e13dbbcdcec27f13a03e365bf74f8f2bd
olthapage <olthar.rosyeda01@gmail.com>
2024-05-07 10:11:41 +0700

    minggu11_Percobaan2_25

--- src/minggu11/SingleLinkedList.java
+++ src/minggu11/SingleLinkedList.java
@@ -32,7 +32,7 @@ public class SingleLinkedList {
         }
     }
 
-    void addList(int input) {
+    void addLast(int input) {
         Node ndInput = new Node(input, null);
         if (isEmpty()) {
             head = ndInput;
@@ -59,24 +59,122 @@ public class SingleLinkedList {
         }
     }
 
-    void insertAt(int index, int input) {
+    void insertArt(int index, int input) {
         if (index < 0) {
-            System.out.println("Perbaiki Logikanya! Kalau indeksnya -1 bagaimana???");
+            System.out.println("Perbaiki logikanya! Kalau indeksnya -1 bagaimana?");
         } else if (index == 0) {
             addFirst(input);
         } else {
             Node temp = head;
-            for (int i = 0; i < index - 1 && temp != null; i++) {
+            for (int i = 0; i < index - 1; i++) {
+                if (temp == null) {
+                    System.out.println("Indeks melebihi batas");
+                    return;
+                }
                 temp = temp.next;
             }
-            if (temp != null) {
-                temp.next = new Node(input, temp.next);
-                if (temp.next.next == null) {
-                    tail = temp.next;
+            if (temp == null) {
+                System.out.println("Indeks melebihi batas");
+                return;
+            }
+            temp.next = new Node(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next;
+            }
+        }
+    }
+
+    int getData(int index) {
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    int indexOf(int key) {
+        Node temp = head;
+        int index = 0;
+        while (temp != null && temp.data != key) {
+            temp = temp.next;
+            index++;
+        }
+        if (temp != null) {
+            return index;
+        } else {
+            return -1; 
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else {
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else {
+            if (head.data == key) {
+                removeFirst();
+                return;
+            }
+            Node temp = head;
+            while (temp.next != null && temp.next.data != key) {
+                temp = temp.next;
+            }
+            if (temp.next != null) {
+                temp.next = temp.next.next;
+                if (temp.next == null) {
+                    tail = temp;
                 }
             } else {
-                System.out.println("Index out of bounds");
+                System.out.println("Elemen tidak ditemukan");
             }
+        }
+    }
+
+    void removeAt(int index) {
+        if (index < 0) {
+            System.out.println("Indeks tidak valid");
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; temp != null && i < index - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null || temp.next == null) {
+            System.out.println("Indeks melebihi batas");
+            return;
+        }
+        temp.next = temp.next.next;
+        if (temp.next == null) {
+            tail = temp;
         }
     }
 }
