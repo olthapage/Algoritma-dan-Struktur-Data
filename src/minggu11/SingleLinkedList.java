b068b218618554132e71831d08845d583f4963bb
olthapage <olthar.rosyeda01@gmail.com>
2024-05-07 08:57:03 +0700

    minggu11_percobaan1_25

---
+++ src/minggu11/SingleLinkedList.java
@@ -0,0 +1,82 @@
+package minggu11;
+
+public class SingleLinkedList {
+    Node head, tail;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            System.out.print("Isi Linked List:");
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println();
+        } else {
+            System.out.println("Linked List Kosong");
+        }
+    }
+
+    void addFirst(int input) {
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    void addList(int input) {
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    void insertAfter(int key, int input) {
+        Node ndInput = new Node(input, null);
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput;
+                }
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    void insertAt(int index, int input) {
+        if (index < 0) {
+            System.out.println("Perbaiki Logikanya! Kalau indeksnya -1 bagaimana???");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1 && temp != null; i++) {
+                temp = temp.next;
+            }
+            if (temp != null) {
+                temp.next = new Node(input, temp.next);
+                if (temp.next.next == null) {
+                    tail = temp.next;
+                }
+            } else {
+                System.out.println("Index out of bounds");
+            }
+        }
+    }
+}
