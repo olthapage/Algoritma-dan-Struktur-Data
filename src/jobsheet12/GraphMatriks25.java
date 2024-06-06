6e3f3c88069c0debb9731e02cc4a24461d36678a
olthapage <olthar.rosyeda01@gmail.com>
2024-06-06 18:29:02 +0700

    Jobsheet12_Percobaan2_Pertanyaan4_25

--- src/jobsheet12/GraphMatriks25.java
+++ src/jobsheet12/GraphMatriks25.java
@@ -26,10 +26,33 @@ public class GraphMatriks25 {
         for (int i = 0; i < vertex; i++) {
             System.out.println("Gedung " + (char) ('A' + i) + ": ");
             for (int j = 0; j < vertex; j++) {
-                System.out.println("Gedung " + (char) ('A' + j) + "(" + matriks[i][j] + "m)");
+                if (matriks[i][j] != 0) {
+                    System.out.println("Gedung " + (char) ('A' + j) + "(" + matriks[i][j] + "m)");
+                }
             }
             System.out.println();
         }
+    }
+
+    public void degree(int asal) {
+        int inDegree = 0;
+        int outDegree = 0;
+
+        for (int i = 0; i < vertex; i++) {
+            if (matriks[i][asal] != 0) {
+                inDegree++;
+            }
+        }
+
+        for (int j = 0; j < vertex; j++) {
+            if (matriks[asal][j] != 0) {
+                outDegree++;
+            }
+        }
+
+        System.out.println("InDegree dari Gedung " + (char) ('A' + asal) + ": " + inDegree);
+        System.out.println("OutDegree dari Gedung " + (char) ('A' + asal) + ": " + outDegree);
+        System.out.println("Degree dari Gedung " + (char) ('A' + asal) + ": " + (inDegree + outDegree));
     }
 
     public static void main(String[] args) {
@@ -47,5 +70,10 @@ public class GraphMatriks25 {
         System.out.println("Hasil setelah penghapusan edge:");
         gdg.removeEdge(2, 1);
         gdg.printGraph();
+
+        System.out.println("Degree dari setiap Gedung:");
+        for (int i = 0; i < 4; i++) {
+            gdg.degree(i);
+        }
     }
 }
