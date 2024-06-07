6bd9834fbab3a5b79324fe5a5f4faf76b0e174b1
olthapage <olthar.rosyeda01@gmail.com>
2024-06-07 15:10:33 +0700

    jobsheet13_Percobaan2_Pertanyaan

--- src/jobsheet13/LoopCollection25.java
+++ src/jobsheet13/LoopCollection25.java
@@ -2,43 +2,50 @@ package jobsheet13;
 
 import java.util.Iterator;
 import java.util.Stack;
+import java.util.Collections;
 
 public class LoopCollection25 {
-    public static void main(String[] args) {
-        Stack<String> fruits = new Stack<>();
-        fruits.push("Banana");
-        fruits.add("Orange");
-        fruits.add("Watermelon");
-        fruits.add("Leci");
-        fruits.push("Salak");
+  public static void main(String[] args) {
+    Stack<String> fruits = new Stack<>();
+    fruits.push("Banana");
+    fruits.add("Orange");
+    fruits.add("Watermelon");
+    fruits.add("Leci");
+    fruits.push("Salak");
 
-        for (String fruit : fruits) {
-            System.out.printf("%s ", fruit);
-        }
-        System.out.println("\n" + fruits.toString());
-
-        while (!fruits.empty()) {
-            System.out.printf("%s ", fruits.pop());
-        }
-        System.out.println();
-
-        fruits.push("Melon");
-        fruits.push("Durian");
-
-        for (Iterator<String> it = fruits.iterator(); it.hasNext();) {
-            String fruit = it.next();
-            System.out.printf("%s ", fruit);
-        }
-        System.out.println();
-
-        fruits.stream().forEach(e -> {
-            System.out.printf("%s ", e);
-        });
-        System.out.println();
-
-        for (int i = 0; i < fruits.size(); i++) {
-            System.out.printf("%s ", fruits.get(i));
-        }
-        System.out.println();
+    for (String fruit : fruits) {
+      System.out.printf("%s ", fruit);
     }
+    System.out.println("\n" + fruits.toString());
+
+    while (!fruits.empty()) {
+        System.out.printf("%s ", fruits.pop());
+    }
+
+    fruits.push("Melon");
+    fruits.push("Durian");
+    fruits.push("Strawberry"); 
+    fruits.add("Mango");  
+    fruits.add("Guava");
+    fruits.add("Avocado");
+
+    Collections.sort(fruits);
+
+    System.out.println("\nFruits after sorting :");
+    for (Iterator<String> it = fruits.iterator(); it.hasNext();) {
+      String fruit = it.next();
+      System.out.printf("%s ", fruit);
+    }
+    System.out.println();
+
+    fruits.stream().forEach(e -> {
+        System.out.printf("%s ", e);
+    });
+    System.out.println();
+
+    for (int i = 0; i < fruits.size(); i++) {
+      System.out.printf("%s ", fruits.get(i));
+    }
+    System.out.println();
+  }
 }
