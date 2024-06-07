0c855302cd4b9a928a0e1d68bbf06bcf364033c0
olthapage <olthar.rosyeda01@gmail.com>
2024-06-07 14:10:50 +0700

    jobsheet13_Percobaan1_25

---
+++ src/jobsheet13/ContohList25.java
@@ -0,0 +1,33 @@
+package jobsheet13;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ContohList25 {
+    public static void main(String[] args) {
+        List<Object> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add("Cireng");
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+        
+        l.add(4);
+        l.remove(0);
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+
+
+        List<String> names = new LinkedList<>();
+        names.add("Noureen");
+        names.add("Akhleenma");
+        names.add("Shannum");
+        names.add("Uwais");
+        names.add("Al-Qarni");
+
+        System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
+        names.set(0, "My kid");
+        System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
+        System.out.println("Names: " + names.toString());
+    }
+}
