420c2c201dbff576ab498ea2f2ae23df7d92e2e5
olthapage <olthar.rosyeda01@gmail.com>
2024-06-07 14:32:46 +0700

    jobsheet13_Percobaan1_modifikasipertanyaan_25

--- src/jobsheet13/ContohList25.java
+++ src/jobsheet13/ContohList25.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class ContohList25 {
     public static void main(String[] args) {
-        List<Object> l = new ArrayList<>();
+
+        List<Integer> l = new ArrayList<>();
         l.add(1);
         l.add(2);
         l.add(3);
-        l.add("Cireng");
-        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %d\n", l.get(0), l.size(), l.get(l.size() - 1));
         
         l.add(4);
         l.remove(0);
-        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %d\n", l.get(0), l.size(), l.get(l.size() - 1));
 
 
-        List<String> names = new LinkedList<>();
+        LinkedList<String> names = new LinkedList<>();
         names.add("Noureen");
         names.add("Akhleenma");
         names.add("Shannum");
@@ -28,6 +28,11 @@ public class ContohList25 {
         System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
         names.set(0, "My kid");
         System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size() - 1));
+        System.out.println("Names: " + names.toString());
+        
+
+        names.push("Mei-mei");
+        System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.getFirst(), names.size(), names.getLast());
         System.out.println("Names: " + names.toString());
     }
 }
